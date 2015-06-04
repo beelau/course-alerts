a3ee4b380c795375e3104b3b403841838dec2019
Brian <mrhawkman@gmail.com>
2015-06-03 21:38:57 -0700

    backend script added

--- regcourse/config.py
+++ regcourse/config.py
@@ -1,6 +1,6 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SECRET_KEY = '131dkllj1312'
+SECRET_KEY = 'hard-to-guess-key'
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
--- regcourse/regcourse.py
+++ regcourse/regcourse.py
@@ -1,16 +1,72 @@
-from sqlalchemy import *
-from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker, Query
 from config import SQLALCHEMY_DATABASE_URI
+from sqlalchemy.orm import relationship, backref
+import time
+import dryscrape
+import re
 
-
+# initiate database connection
 db = create_engine("%s" % SQLALCHEMY_DATABASE_URI)
-sess = sessionmaker(db)
-sess = sess.configure(db)
+base = declarative_base()
+base.metadata.reflect(db)
 
+# Classes for accesses
+class Course(base):
+	__table__ = base.metadata.tables['courses']
 
-def run(stmt):
-	rs = stmt.execute()
-	for row in rs:
-		print row
+class User(base):
+	__table__ = base.metadata.tables['users']
 
-courses = sess.query(Course).all()
+# Methods
+def startSession():
+	db_session = scoped_session(sessionmaker(bind=db))
+	course_list = db_session.query(Course).all()
+	regCourse(course_list)
+
+def regCourse(course_list):
+
+	print "start"	
+	for c in course_list:
+		id = c.id
+		cname = c.cname
+		cid = c.cid
+		sec = c.sec
+		print "%d - %s, %d, %s" % (id, cname, cid, sec)
+
+		sess = dryscrape.Session()
+		sess.set_attribute('auto_load_images', False)
+		sess.visit("https://courses.students.ubc.ca/cs/main?sessyr=2014&sesscd=W")
+		sess.at_xpath('//*[@id="ubc7-unit-navigation"]/ul/li[1]/div/a').click()
+		sess.at_xpath('//*[@id="ubc7-unit-navigation"]/ul/li[1]/div/ul/li[2]/a').click()
+		sess.visit("https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=5&dept=%s&course=%d&section=%s" % (cname, cid, sec))
+		pagehtml = sess.source()
+		findSeats(pagehtml)
+	print "end"
+
+def findSeats(pagehtml):
+	seatsRE = '<td width="200px">Total Seats Remaining:</td><td align="left"><strong>(\d+)</strong></td>' + '.+' + \
+				'<td width="200px">General Seats Remaining:</td><td align="left"><strong>(\d+)</strong>' + '.+' + \
+				'<td width="200px">Restricted Seats Remaining\*:</td><td align="left"><strong>(\d+)</strong></td>'
+	seats = re.search(seatsRE, pagehtml)
+	totalseats = int(seats.group(1))
+	generalseats = int(seats.group(2))
+	reservedseats = int(seats.group(3))
+
+	room = 0
+	reserved = 'f'
+	if totalseats > 0:
+		if generalseats > 0:
+			room = generalseats
+		elif reservedseats > 0:
+			if reserved == 'y':
+				room = reservedseats
+	
+def main():
+	start_time = time.time()
+	startSession()
+	print("--- %s seconds ---" % (time.time() - start_time))
+
+main()
